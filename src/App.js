001ac9fabf25d2fe234a7726fe772dd10e4854c2
Alisdair Ferguson <alisdair@tryzipper.com>
2018-06-28 01:07:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const fontAwesomeStyles = {
+  color: '#24ce99',
+  fontSize: '5em'
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container mt-5" style={{fontSize: '1.3em'}}>
+          <div className="container mb-4">
+            <div className="row justify-content-sm-center">
+              <div className="col-xs-12">
+                <h1 className="text-center" style={{fontWeight: 'bold'}}>Contact Us</h1>
+                <p>If you have a question or need more information, feel free to get in touch with us.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="container">
+            <div className="row justify-content-sm-center">
+              <div className="col-sm-4 mt-5">
+                <i className="far fa-envelope-open" style={fontAwesomeStyles}></i>
+                <div className="mt-5">
+                  <h2>Email us</h2>
+                  <p><a href="mailto:info@tryzipper.com">info@tryzipper.com</a></p>
+                </div>
+              </div>
+             
+              <div className="col-sm-4 mt-5">
+                <i className="far fa-comments" style={fontAwesomeStyles}></i>
+                <div className="mt-5">
+                  <h2>Chat with us</h2>
+                  <p><a href="https://drift.me/zipper">Live chat</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
